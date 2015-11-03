72a1e007ab3c6c13c7aab3decffaf8deba5f1681
farodin91 <farodin91@googlemail.com>
2015-11-04 00:41:13 +0100

    testing ffi

--- build.rs
+++ build.rs
@@ -1,23 +1,9 @@
-extern crate gcc;
-use std::process::Command;
-use std::path::Path;
-use std::env;
 
 fn main() {
-    let out_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("lib/sqlite4/build");
-    //panic!("{:?}", dest_path);
-    //TODO fossil pull
-    //TODO fossil open
-    Command::new("cp").arg(format!("{:?}/Makefile.linux-gcc",dest_path)).arg(format!("{:?}/Makefile",dest_path)).status().unwrap_or_else(|e| {
-        panic!("failed to execute process: {}", e)
-    });
+    let lib_dir = "/usr/lib/libsqlite4.a";
+    let include_dir = "/usr/include/sqlite4.h";
 
-
-    let dest2_path = Path::new(&out_dir).join("lib/sqlite4/build");
-    Command::new("make").status().unwrap_or_else(|e| {
-        panic!("failed to execute process: {}", e)
-    });
-
-    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-search=native={}", lib_dir);
+    println!("cargo:rustc-link-lib=sqlite4");
+    println!("cargo:include={}", include_dir);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+extern crate libc;
+
+
+pub enum Sqlite4Env {}
+pub enum Sqlite4 {}
+
+#[link(name = "sqlite4")]
+extern {
+    fn sqlite4_close() -> i32;
+    fn sqlite4_open(pEnv: *mut Sqlite4Env, filename: &str, handle: *mut Sqlite4) -> i32;
+    fn sqlite4_env_default() -> *mut Sqlite4Env;
+}
+
+pub fn db_close() -> i32{
+    unsafe {
+        sqlite4_close()
+    }
+}
+
+pub fn db_open(pEnv: *mut Sqlite4Env, filename: &str, handle: *mut Sqlite4) -> i32{
+    unsafe {
+        sqlite4_open(env, filename, handle)
+    }
+}
 
 fn main() {
-    println!("status");
+    let mut rust_object = Box::new(Sqlite4);
+    let env = unsafe{ sqlite4_env_default() };
+    println!("status {:?}",db_open(env,"test.db",rust_object));
 }
